8a9940c3347d02523fea09a21b048257ea5948ab
seung hyun han <han1448@hanmail.net>
2019-10-06 22:27:13 +0900

    Update SpringBoot 2.2.0-RC1 Update pom

--- client/src/main/java/client/ClientConfiguration.java
+++ client/src/main/java/client/ClientConfiguration.java
@@ -1,38 +1,17 @@
 package client;
 
-import io.rsocket.RSocket;
-import io.rsocket.RSocketFactory;
-import io.rsocket.frame.decoder.PayloadDecoder;
-import io.rsocket.transport.netty.client.TcpClientTransport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.rsocket.MetadataExtractor;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.messaging.rsocket.RSocketStrategies;
-import org.springframework.util.MimeTypeUtils;
 
 @Configuration
 public class ClientConfiguration {
-
-    /**
-     * Set connection to RSocket server.
-     *
-     * @return RSocket
-     */
-    @Bean
-    public RSocket rSocket() {
-        return RSocketFactory
-                .connect()
-                .mimeType(MetadataExtractor.ROUTING.toString(), MimeTypeUtils.APPLICATION_JSON_VALUE)
-                .frameDecoder(PayloadDecoder.ZERO_COPY)
-                .transport(TcpClientTransport.create(7000))
-                .start()
-                .block();
-    }
-
     @Bean
     RSocketRequester rSocketRequester(RSocketStrategies rSocketStrategies) {
-        return RSocketRequester
-                .wrap(rSocket(), MimeTypeUtils.APPLICATION_JSON, MetadataExtractor.ROUTING, rSocketStrategies);
+        return RSocketRequester.builder()
+                .rsocketStrategies(rSocketStrategies)
+                .connectTcp("localhost", 7000)
+                .block();
     }
 }
